64b1f4e4c98bcf45aeb86ad1975c5aee35fbcee4
johnwick020213 <ganbo020213@gmail.com>
2024-07-02 11:23:15 +0800

    添加sendn和recvn

--- src/network/NetworkCommunicator.h
+++ src/network/NetworkCommunicator.h
@@ -7,13 +7,15 @@ class NetworkCommunicator
 {
 public:
     NetworkCommunicator();
-    
+    ~NetworkCommunicator();
+
     void sendMessage(const std::string&msg,const std::string& address);
 
     std::string receiveMessage();
 private:
     int socket;
-
+    bool sendn(int sockfd, const char* buf, int len);
+    bool recvn(int sockfd, char* buf, int len);
 };
 
 #endif
