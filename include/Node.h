9c1c3e55dbce8084b30904b84f669c00c1e1ae27
johnwick020213 <ganbo020213@gmail.com>
2024-07-02 19:30:45 +0800

    创建 Node.h

---
+++ include/Node.h
@@ -0,0 +1,20 @@
+#ifndef NODE_H
+#define NODE_H
+
+
+class Node
+{
+public:
+    Node(int id);
+    
+    void start();//启动节点
+    void stop();//停止节点
+    int getId();//获取节点ID
+
+private:
+    int id;
+    bool running;//节点运行状态
+};
+
+
+#endif
