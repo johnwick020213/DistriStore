2fd224ec14d476c19b002d7b5214e28018dd4c7a
johnwick020213 <ganbo020213@gmail.com>
2024-07-04 15:41:41 +0800

    网络服务搭建

--- server/include/network/NetworkHandler.h
+++ server/include/network/NetworkHandler.h
@@ -26,6 +26,15 @@ private:
     std::string address; // 网络地址
     int port;            // 端口号
     int socketFd;        // 套接字文件描述符
+
+    // 创建套接字并连接到指定地址和端口
+    int createSocket(const std::string& address, int port);
+
+    // 发送指定长度的数据
+    int sendn(int socket, const char* buf, int len);
+
+    // 接收指定长度的数据
+    int recvn(int socket, char* buf, int len);
 };
 
 #endif // NETWORK_HANDLER_H
--- server/src/network/NetworkHandler.cpp
+++ server/src/network/NetworkHandler.cpp
@@ -1,22 +1,93 @@
-#include"NetworkCommunicator.h"
+#include"../../include/network/NetworkHandler.h"
 #include <iostream>
 #include<cstring>
 #include<sys/socket.h>
 #include<arpa/inet.h>
 #include<unistd.h>
 
-NetworkCommunicator::NetworkCommunicator(){
-   socket=::socket(AF_INET,SOCK_STREAM,0) ;
-   if(socket==-1){
-       std::cerr<<"创建套接字失败"<<std::endl;
-   }
+//构造函数初始化网络服务
+NetworkHandler::NetworkHandler(const std::string& address,int port)
+:address(address),port(port),socketFd(-1)
+{}
+
+//创建套接字链接到指定端口
+int NetworkHandler::createSocket(const std::string& address,int port)
+{
+    int sockfd=socket(AF_INET,SOCK_STREAM,0);
+    if(sockfd<0){
+        std::cerr<<"错误：创建套接字失败"<<std::endl;
+        return -1;
+    }
+
+    struct sockaddr_in serverAddr;
+    memset(&serverAddr,0,sizeof(serverAddr));
+    serverAddr.sin_family=AF_INET;
+    serverAddr.sin_port=htons(port);
+    if(inet_pton(AF_INET,address.c_str(),&serverAddr.sin_addr)<=0){
+        std::cerr<<"错误：未知地址"<<std::endl;
+        close(sockfd);
+        return -1;
+    }
+
+    if(connect(sockfd,(struct sockaddr*)&serverAddr,sizeof(serverAddr))<0){
+        std::cerr<<"错误：连接失败"<<std::endl;
+        close(sockfd);
+        return -1;
+    }
+    
+    return sockfd;
 }
 
-NetworkCommunicator::~NetworkCommunicator(){
-    close(socket);
+//发送数据到指定端口
+bool NetworkHandler::sendData(const std::string&address,int port,const std::string &data)
+{
+    int sockfd = createSocket(address,port);
+    if(sockfd<0){
+        return false;
+    }
+
+    int dataLength=data.size();
+    if(sendn(sockfd,(char*)&dataLength,sizeof(dataLength))==-1){
+        std::cerr<<"错误：获得数据长度失败"<<std::endl;
+        close(sockfd);
+        return false;
+    }
+
+    if(sendn(sockfd,data.c_str(),dataLength)==-1)
+    {
+        std::cerr<<"错误：发送数据失败"<<std::endl;
+        close(sockfd);
+        return false;
+    }
+
+    close(sockfd);
+    return true;
 }
 
-bool NetworkCommunicator::sendn(int sockfd,const char * buf,int len){
+
+//接收数据
+std::string NetworkHandler::receiveData(){
+    int dataLength;
+    if(recvn(socketFd,(char*)&dataLength,sizeof(dataLength))==-1){
+        std::cerr<<"错误：接收数据长度失败"<<std::endl;
+        return "";
+    }
+
+    char* buffer=new char[dataLength+1];
+    if(recvn(socketFd,buffer,dataLength)==-1){
+        std::cerr<<"错误：接收数据失败"<<std::endl;
+        delete []buffer;
+        return "";
+    }
+
+    buffer[dataLength]='\0';
+    std::string data(buffer);
+    delete []buffer;
+    return data;
+}
+
+
+int NetworkHandler::sendn(int sockfd,const char * buf,int len){
     int total=0;//已发送字节
     int byteleft=len;//剩余需要发送的字节数
     int n;
@@ -32,7 +103,7 @@ bool NetworkCommunicator::sendn(int sockfd,const char * buf,int len){
     return n!=-1;
 }
 
-bool NetworkCommunicator::recvn(int sockfd,char*buf,int len){
+int NetworkHandler::recvn(int sockfd,char*buf,int len){
      int total = 0; // 已接收的字节数
     int bytesleft = len; // 剩余需要接收的字节数
     int n;
@@ -49,58 +120,18 @@ bool NetworkCommunicator::recvn(int sockfd,char*buf,int len){
     return n != -1 && n != 0;
 }
 
-void NetworkCommunicator::sendMessage(const std::string&msg,const std::string& address){
-    //设置服务器地址
-    sockaddr_in serverAddr;
-    serverAddr.sin_family=AF_INET;
-    serverAddr.sin_port=htons(8080);
-
-    //地址字符串转换为网络地址
-    if(inet_pton(AF_INET,address.c_str(),&serverAddr.sin_addr)<=0){
-        std::cerr<<"地址不支持或错误"<<std::endl;
-        return;
+//开启网络服务
+void NetworkHandler::startService(){
+    socketFd=createSocket(address,port);
+    if(socketFd<0){
+        std::cerr<<"错误：网络服务启动失败"<<std::endl;
     }
-
-    //连接服务器
-    if(connect(socket,(struct sockaddr*)&serverAddr,sizeof(serverAddr))<0){
-        std::cerr<<"连接服务器失败"<<std::endl;
-        return;
-    }
-
-    //发送消息长度
-    int msgLen=msg.size();
-    if(!sendn(socket,(char*)&msgLen,sizeof(msgLen))){
-        std::cerr<<"发送消息长度失败"<<std::endl;
-        return;
-    }
-
-    //发送信息
-    if(!sendn(socket,msg.c_str(),msgLen)){
-        std::cerr<<"发送消息内容失败"<<std::endl;
-        return;
-    }
-
-    std::cout<<"消息发送到： "<<msg<<std::endl;
-
 }
 
-std::string NetworkCommunicator::receiveMessage(){
-    //接收消息长度
-    int msgLen;
-    if(!recvn(socket,(char*)&msgLen,sizeof(msgLen))){
-        std::cerr<<"接收长度失败"<<std::endl;
-        return "";
+//关闭网络服务
+void NetworkHandler::stopService(){
+    if(socketFd>=0){
+        close(socketFd);
+        socketFd=-1;
     }
-
-    //接收消息内容
-    char* buffer=new char[msgLen];
-    if(!recvn(socket,buffer,msgLen)){
-        std::cerr<<"接收内容失败"<<std::endl;
-        delete[]buffer;
-        return "";
-    }
-    std::string msg(buffer,msgLen);
-    delete[]buffer;
-    std::cout<<"消息接收："<<msg<<std::endl;
-    return msg;
 }
